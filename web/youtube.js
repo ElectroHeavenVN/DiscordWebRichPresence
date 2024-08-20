9fe49b44f47c68ab850750c4521f5d879d17ae8a
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-08-21 02:13:07 +0700

    YouTube: Use Dearrow title when available and displayed

--- web/youtube.js
+++ web/youtube.js
@@ -38,10 +38,16 @@ function refreshInfo() {
             var shortsVideoElement = Array.from(document.querySelector("#shorts-inner-container").querySelectorAll("ytd-reel-video-renderer")).find(r => r.hasAttribute("is-active"));
         if (document.querySelector(".html5-video-player") != null)
             playing = document.querySelector(".html5-video-player#" + (isYTShorts ? "shorts-player" : "movie_player")).classList.contains("playing-mode");
-        if (isYTShorts && shortsVideoElement != null)
+        var originalTitleElement = document.querySelector("#info .title");
+        if (originalTitleElement != null) {
+            var dearrowTitleElement = document.querySelector(".cbCustomTitle");
+            if (dearrowTitleElement != null && dearrowTitleElement.style.display != "none")
+                title = dearrowTitleElement.innerText;
+            else if (originalTitleElement.style.display != "none")
+                title = originalTitleElement.innerText;
+        }
+        else if (isYTShorts && shortsVideoElement != null)
             title = shortsVideoElement.querySelector("reel-player-header-renderer > h2").innerText;
-        else if (document.querySelector("#info .title") != null)
-            title = document.querySelector("#info .title").innerText;
         var isLiveStreaming = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate.ytp-live > button") != null;
         var videoPlayer = document.querySelector(".html5-video-player#" + (isYTShorts ? "shorts-player" : "movie_player") + " video");
         if (videoPlayer == null)
